498ce25dd50f8e26659bf458313ff66665ab6d8d
uryyyyyyy <koki305@gmail.com>
2017-01-25 20:54:24 +0900

    tmp

--- scala/front/src/main/scala/com/github/uryyyyyyy/universal_scalajs/front/ToDoReduxModule.scala
+++ scala/front/src/main/scala/com/github/uryyyyyyy/universal_scalajs/front/ToDoReduxModule.scala
@@ -1,75 +1,45 @@
 package com.github.uryyyyyyy.universal_scalajs.front
 
-import com.github.uryyyyyyy.universal_scalajs.domain.ToDo
-import org.scalajs.dom.experimental.{Fetch, ReadableStream}
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.annotation.{JSExport, ScalaJSDefined}
-import scala.scalajs.js.typedarray.Uint8Array
-import io.circe._
-import io.circe.parser._
+import scala.util.Random
 
-
-// ---------------
-// Entry Point
-// ---------------
 
 @JSExport("ToDoReduxModule")
 object ToDoReduxModule {
 
-  @ScalaJSDefined
-  trait MyJsTrait extends js.Object {
-    val name: String
-    val id: Int
+  @JSExport
+  def helloWorld(): Seq[Int] = {
+    js.Array(1, 2, 4).toSeq
   }
 
   @JSExport
-  def helloWorld(): js.Object = {
-    new MyJsTrait {
-      val id = 5
-      val name = "haha"
+  def reducer: js.Function2[js.Array[ToDoVO], ActionBase, js.Array[ToDoVO]] = {
+    val scalaFun: (js.Array[ToDoVO], ActionBase) => js.Array[ToDoVO] = (state: js.Array[ToDoVO], action: ActionBase) => {
+      action match {
+        case a: AddTodoAction =>
+          val seq = state.toSeq
+          val newSeq = seq :+ a.todo
+          newSeq.toJSArray
+        case _ =>
+          if(state == null){
+            js.Array[ToDoVO]()
+          }else{
+            state
+          }
+      }
     }
-  }
-
-  @JSExport
-  def reducer(
-    state :js.Array[ToDoVO],
-    action: ActionBase
-  ): js.Array[ToDoVO] = {
-    if(state == null) return js.Array.apply()// initialState
-
-    if(action.`type` == "ADD_TODO"){
-      state
-    }else{
-      state
-    }
-  }
-
-  @JSExport
-  def fetch2(url :String): js.Promise[ReadableStream[Uint8Array]] = {
-    Fetch.fetch(url).toFuture.map(r => {
-      println(r.body)
-      r.body
-    }).toJSPromise
+    scalaFun
   }
 
   @JSExport
   def createToDoActionDispatcher(
-    dispatch: js.Function1[ActionBase, js.Any]
-    //todoList: List[ToDo]
+    dispatch: js.Function1[ActionBase, js.Any],
+    todoList: js.Array[ToDoVO]
   ): ToDoActionDispatcher = {
-    new ToDoActionDispatcher(dispatch)
+    new ToDoActionDispatcher(dispatch, todoList)
   }
-
-  @JSExport
-  def circeSample(rawJson: String): Unit = {
-    val parseResult = parse(rawJson)
-    println(parseResult)
-  }
-
-
 }
 
 
@@ -78,30 +48,31 @@ object ToDoReduxModule {
 // ---------------
 
 class ToDoActionDispatcher(
-  val dispatch: js.Function1[ActionBase, js.Any]
+  val dispatch: js.Function1[ActionBase, js.Any],
+  todoList: js.Array[ToDoVO]
 ){
   @JSExport
-  def addToDoAction(todo: ToDoJSON): Unit ={
-    val actionObject = js.Dynamic.literal(`type` = "ADD_TODO", todo = todo).asInstanceOf[ActionBase]
+  def addToDoAction(_title: String): Unit ={
+    val seed = new Random()
+    val actionObject = new AddTodoAction{
+      val todo = new ToDoVO{
+        val id = seed.nextInt().toString
+        val title = _title
+        val isFinished = false
+      }
+    }
     this.dispatch(actionObject)
   }
 }
 
-@js.native
-trait ActionBase extends js.Object {
-  val `type`: String = js.native
+sealed trait ActionBase
+trait AddTodoAction extends ActionBase {
+  val todo: ToDoVO
 }
 
-@js.native
-trait ToDoJSON extends js.Object {
-  val isFinished: Boolean = js.native
-  val title: String = js.native
-}
-
-case class ToDoVO(id: String, title: String, isFinished: Boolean){
-  @JSExport
-  def getTitle():String = this.title
-
-  @JSExport
-  def isEqual(obj: scala.Any): Boolean = this == obj
+@ScalaJSDefined
+trait ToDoVO extends js.Object {
+  val id: String
+  val title: String
+  val isFinished: Boolean
 }
