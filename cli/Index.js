5d836c70f5a501e98f686f7723d7d7bf48486faa
uryyyyyyy <koki305@gmail.com>
2017-01-25 20:54:48 +0900

    tmp

--- cli/Index.js
+++ cli/Index.js
@@ -1,31 +1,12 @@
 const {ToDoReduxModule} = require("./scalajsDist/front-fastopt");
 
 const myModule = ToDoReduxModule();
-// console.log(myModule.func1());
-// const todo = ToDoVO("title", false);
-// console.log(todo.toString());
-//
-// console.log(myModule.func1());
 
-const aa = myModule.helloWorld()
-console.log(aa.name)
-console.log(aa)
+let action = null;
 
-// const json = `
-// {
-//   "foo": "bar",
-//   "baz": 123,
-//   "list of stuff": [ 4, 5, 6 ]
-// }`
-//
-// const json2 = {foo: "bar", baz: 123, "list of stuff": [ 4, 5, 6 ]}
-//
-// myModule.circeSample(JSON.stringify(json2));
+console.log(myModule.reducer)
 
-// const dispatch = (a) => console.log(a);
-// const actionDispatcher = myModule.createToDoActionDispatcher(dispatch)
-//
-// const obj = {isFinished: false, title: "haha"}
-// actionDispatcher.addToDoAction(obj);
-//
-// // myModule.reducer({type: "ADD_TODO"})
+const toDoActionDispatcher = myModule.createToDoActionDispatcher((a) => action = a, [])
+toDoActionDispatcher.addToDoAction("title2")
+const result = myModule.reducer([{id: "id1", title: "title1", isFinished: true}], action)
+console.log(result)
